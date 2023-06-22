fcca46198fa448152dfc771891325ccb6051ba43
Akhil.V.K <126905134+AkhilVK45678998765@users.noreply.github.com>
2023-06-22 17:17:10 +0530

    Add files via upload

---
+++ supplyapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+
+
+admin.site.register(shopregmodel)
+admin.site.register(regmodel)
+admin.site.register(productmodel)
+admin.site.register(buymodel)
+admin.site.register(feedmodel)
+
